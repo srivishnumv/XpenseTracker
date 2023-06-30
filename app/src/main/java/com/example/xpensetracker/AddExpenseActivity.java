d27133dd7370d6f1b828de6d08fad3c16a39dd27
Srivishnu MV <srivishnum.v06@gmail.com>
2023-06-30 11:39:28 +0530

    FireStore Integration Succesfull

--- app/src/main/java/com/example/xpensetracker/AddExpenseActivity.java
+++ app/src/main/java/com/example/xpensetracker/AddExpenseActivity.java
@@ -1,17 +1,98 @@
 package com.example.xpensetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.xpensetracker.databinding.ActivityAddExpenseBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class AddExpenseActivity extends AppCompatActivity {
     ActivityAddExpenseBinding binding;
+    FirebaseFirestore fstore;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String type="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= ActivityAddExpenseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        fstore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        binding.expenseRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type="Expense";
+                binding.expenseRadio.setChecked(true);
+                binding.incomeRadio.setChecked(false);
+            }
+        });
+        binding.incomeRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type="Income";
+                binding.expenseRadio.setChecked(false);
+                binding.incomeRadio.setChecked(true);
+            }
+        });
+        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String amount=binding.amount.getText().toString().trim();
+                String note=binding.note.getText().toString().trim();
+                String category = binding.category.getText().toString().trim();
+                if(amount.length()<=0){
+                    return;
+                }
+
+                if(type.length()<=0){
+                    Toast.makeText(AddExpenseActivity.this,"Select Transaction type",Toast.LENGTH_SHORT).show();
+
+                }
+                String id=UUID.randomUUID().toString();
+                Map<String,Object>  transaction = new HashMap<>();
+                transaction.put("id",id);
+                transaction.put("amount",amount);
+                transaction.put("note",note);
+                transaction.put("type",type);
+
+
+                fstore.collection("Expenses").document(firebaseAuth.getUid()).collection("Notes")
+                        .document(id)
+                        .set(transaction)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(AddExpenseActivity.this,"Added Succesfully",Toast.LENGTH_SHORT).show();
+                                binding.note.setText("");
+                                binding.amount.setText("");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AddExpenseActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/xpensetracker/DashboardActivity.java
+++ app/src/main/java/com/example/xpensetracker/DashboardActivity.java
@@ -1,10 +1,19 @@
 package com.example.xpensetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.xpensetracker.databinding.ActivityDashboardBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
     ActivityDashboardBinding binding;
@@ -13,5 +22,51 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Intent intent=new Intent(DashboardActivity.this,AddExpenseActivity.class);
+        binding.addIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                intent.putExtra("type","Income");
+                startActivity(intent);
+
+            }
+        });
+        binding.addExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                intent.putExtra("type","Expense");
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    protected void onStart(){
+        super.onStart();
+        ProgressDialog progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Please");
+        progressDialog.setMessage("Wait");
+        progressDialog.setCancelable(false);
+
+        if(FirebaseAuth.getInstance().getCurrentUser()==null)
+        {
+            progressDialog.show();
+            FirebaseAuth.getInstance().signInAnonymously()
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.cancel();
+                            Toast.makeText(DashboardActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
