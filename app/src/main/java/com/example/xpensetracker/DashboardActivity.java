671570b93b0e14d8c132b1833533cc479962a265
Srivishnu MV <srivishnum.v06@gmail.com>
2023-07-01 18:20:31 +0530

    refresh bug fixed

--- app/src/main/java/com/example/xpensetracker/DashboardActivity.java
+++ app/src/main/java/com/example/xpensetracker/DashboardActivity.java
@@ -75,6 +75,7 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(DashboardActivity.this,DashboardActivity.class));
+                finish();
             }
         });
         loadData();
