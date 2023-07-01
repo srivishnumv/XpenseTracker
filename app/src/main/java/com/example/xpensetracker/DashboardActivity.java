421ea5a2b1231a65753ad22816eacecf5a970f33
Srivishnu MV <srivishnum.v06@gmail.com>
2023-07-01 17:27:52 +0530

    PieChart Graph

--- app/src/main/java/com/example/xpensetracker/DashboardActivity.java
+++ app/src/main/java/com/example/xpensetracker/DashboardActivity.java
@@ -11,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.xpensetracker.databinding.ActivityDashboardBinding;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -20,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.github.mikephil.charting.data.PieDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +33,8 @@ public class DashboardActivity extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firestore;
 
+    int sumExpense=0;
+    int sumIncome=0;
     ArrayList<ExpenseModel> expenseModelArrayList;
     ExpenseAdaptor expenseAdaptor;
     @Override
@@ -81,13 +87,45 @@ public class DashboardActivity extends AppCompatActivity {
                                     ds.getString("amount"),
                                     ds.getString("type"),
                                     ds.getString("date"));
+
+                            int amount = Integer.parseInt(ds.getString("amount"));
+                            if(ds.getString("type").equals("Expense")){
+                                sumExpense=sumExpense+amount;
+                            }
+                            else{
+                                sumIncome=sumIncome+amount;
+                            }
                             expenseModelArrayList.add(ExpenseModel);
 
                         }
+
                         expenseAdaptor = new ExpenseAdaptor(DashboardActivity.this,expenseModelArrayList);
                         binding.recycler.setAdapter(expenseAdaptor);
+                        setUpGraph();
                     }
                 });
+
+    }
+
+    private void setUpGraph() {
+        List < PieEntry> pieEntryList = new ArrayList<>();
+        List <Integer> colorsList = new ArrayList<>();
+        if(sumIncome!=0){
+            pieEntryList.add(new PieEntry(sumIncome,"Income"));
+            colorsList.add(getResources().getColor(R.color.green_n1));
+        }
+        if(sumExpense!=0){
+            pieEntryList.add(new PieEntry(sumExpense,"Expense"));
+            colorsList.add(getResources().getColor(R.color.yellow_n1));
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(pieEntryList, String.valueOf(sumIncome-sumExpense));
+        pieDataSet.setColors(colorsList);
+        PieData pieData = new PieData((pieDataSet));
+
+
+        binding.pieChart.setData(pieData);
+        binding.pieChart.invalidate();
 
     }
 
